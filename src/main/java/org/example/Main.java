d36aac9c989112155a4083b2a8bebf418567baca
gr1lzy <kolomoets02@gmail.com>
2024-07-30 16:10:06 +0300

    Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.entity.Block;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Block> blockchain = new ArrayList<>();
 
+        blockchain.add(new Block("Hello World!", "0"));
+        blockchain.add(new Block("Hello Java!", blockchain.getLast().hash));
+        blockchain.add(new Block("Hello Blockchain!", blockchain.getLast().hash));
+
+        System.out.println(Block.isValidHash(blockchain));
     }
 }
